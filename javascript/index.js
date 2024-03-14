45a06d2a5653dd955d6dbf940fe11e99607ea615
azyruz7 <yanngabrieldev@gmail.com>
2024-03-14 18:15:34 -0300

    up

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")){
+        icon.src = "imagens/dark-mode.png";
+    }else{
+        icon.src = "imagens/light-mode.png";
+    }
+}
+
+
+function clickMenu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+}
