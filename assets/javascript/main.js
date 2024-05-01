1a8fd87dafe154c53fd297c0d7dd4be5bd8ca4b7
YannGabriel <yanngabrieldev@gmail.com>
2024-04-30 23:19:03 -0300

    debug light-mode

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,9 +4,9 @@ var icon = document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("light-theme");
     if(document.body.classList.contains("light-theme")){
-        icon.src = "../assets/imagens/dark-mode.png";
+        icon.src = "./assets/imagens/dark-mode.png";
     }else{
-        icon.src = "../assets/imagens/light-mode.png";
+        icon.src = "./assets/imagens/light-mode.png";
     }
 }
 
