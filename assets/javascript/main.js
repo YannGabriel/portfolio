c15ed02072a2a2b2561955869c98356e03ffbfb3
YannGabriel <yanngabrieldev@gmail.com>
2024-07-16 11:22:10 -0300

    de-bug mobile version

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,7 @@ var icon = document.getElementById("icon");
 
 icon.onclick = function() {
     document.body.classList.toggle("light-theme");
-    
+
     var images = document.querySelectorAll("img[data-light-src]");
     images.forEach(function(image) {
         if (document.body.classList.contains("light-theme")) {
@@ -12,5 +12,18 @@ icon.onclick = function() {
         }
     });
 
-    
+    console.log("Theme toggled, current class:", document.body.classList);
 }
+
+const myObserverN = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showN');
+        } else {
+            entry.target.classList.remove('showN');
+        }
+    });
+});
+
+const elementsN = document.querySelectorAll('.hiddenN');
+elementsN.forEach((element) => myObserverN.observe(element));
