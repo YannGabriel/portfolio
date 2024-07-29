65927f05fbf3d82d152a7a12c6be956c3dca8b58
YannGabriel <yanngabrieldev@gmail.com>
2024-07-29 16:49:17 -0300

    test JSON part with name
    This project was finish, so i tried a JSON DATA with only one elemnt, my name :)

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,12 @@
 
+function updateProfileInfos(profileData){
+  const name = document.getElementById('profile-name')
+  console.log("Passei")
+  name.innerText = profileData.name
+}
 
 (async () => {
   const profileData = await fetchProfileData()
-  console.log(profileData)
-})
+  updateProfileInfos(profileData)
+})()
+
