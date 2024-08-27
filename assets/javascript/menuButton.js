a6eb2c142108114be37e2d4380824412a2fc135e
YannGabriel <yanngabrieldev@gmail.com>
2024-08-27 00:57:11 -0300

    update menu button

--- assets/javascript/menuButton.js
+++ assets/javascript/menuButton.js
@@ -1,19 +1,18 @@
 
-const menuButton = document.getElementById("icon")
-const menuMobile = document.getElementById("menuMobile")
-menuMobile.style.display = 'none'
+const menuButton = document.getElementById("icon");
+const menuMobile = document.getElementById("menuMobile");
+menuMobile.style.display = 'none';
 
-function openMenu (){
-    menuButton.addEventListener("click", () => {
-        if (menuMobile.style.display == 'none'){
-            menuMobile.style.display = 'block'
-            console.log("Fechar")
-            console.log("Foi")
-        }
-        else{
-            menuMobile.style.display ='none'
-        }
-    })
+function toggleMenu() {
+    if (menuMobile.style.display === 'none') {
+        menuMobile.style.display = 'block';
+    } else {
+        menuMobile.style.display = 'none';
+    }
 }
+
+menuButton.addEventListener("click", toggleMenu);
+
+
 
 
