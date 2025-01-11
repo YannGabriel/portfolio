e58b8c7e9aefbf02612241b4cb0468634aab6028
YannGabriel <yanngabrieldev@gmail.com>
2025-01-11 14:37:36 -0300

    test for fetch

--- src/components/header/MenuMobile/menuMobile.tsx
+++ src/components/header/MenuMobile/menuMobile.tsx
@@ -1,14 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./menuMobile.module.scss";
 
+
+interface SectionProps{
+  image?: string;
+  name?: string
+  text?: string
+}
+
 export const MobileMenu = () => {
+
+  const [sections, setSection] = useState<SectionProps[]>([])
+
   useEffect(() => {
-    fetch("")
+    fetch("https://raw.githubusercontent.com/YannGabriel/portfolio/refs/heads/main/src/database/api/menuData.json",{
+      method: "GET",
+      headers:{
+        "Content-Type": "ap0lication/json"
+      }
+    }).then((resp) => resp.json())
+    .then((data) =>{
+      setSection(data)
+    })
+    .catch((err) => console.log(err))
   }, []);
 
   return (
     <>
-      {menuSections.map((section, index) => (
+      {sections.map((section, index) => (
         <div key={index}>
           <img src={section.image} alt={section.name} />
           <h2 className={styles.sectionTitle}>{section.text}</h2>
