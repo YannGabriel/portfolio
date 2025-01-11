1657b32b2c0b2fad5759908759789df78b261a2a
YannGabriel <yanngabrieldev@gmail.com>
2025-01-11 17:19:33 -0300

    create more animations

--- src/components/Intro/intro.tsx
+++ src/components/Intro/intro.tsx
@@ -12,7 +12,7 @@ export const Intro = () => {
       <motion.span 
       initial= {{opacity:0}}
       animate= {{ opacity: 1}}
-      transition={{duration: 0.15, delay: 0.1, ease: "linear"}}
+      transition={{duration: 0.15, delay: 1, ease: "linear"}}
       className={styles.welcomeContent}>
         Olá, mundo! Eu sou
       </motion.span>
@@ -21,14 +21,22 @@ export const Intro = () => {
         <Typewriter textInput="Yann Gabriel." />
       </div>
 
-      <span className={styles.prof}>Web Developer</span>
+      <motion.span
+      initial= {{y: -20, opacity:0}}
+      animate= {{y:0, opacity: 1}}
+      transition={{duration: 0.2, delay: 1.5, ease: "linear"}} 
+      className={styles.prof}>Web Developer</motion.span>
 
-      <p className={styles.introText}>
+      <motion.p 
+      initial= {{x: -20, opacity:0}}
+      animate= {{x: 0, opacity: 1}}
+      transition={{duration: 0.25, delay: 2, ease: "linear"}}
+      className={styles.introText}>
         Como desenvolvedor web, estou sempre em busca de novos desafios e
         oportunidades para aplicar minha paixão por tecnologia. Acredito no
         poder da inovação para criar soluções que realmente impactem. Explore
         meu trabalho e conecte-se comigo através dos meus perfis e currículo!
-      </p>
+      </motion.p>
 
       <MediasButtons />
     </div>
