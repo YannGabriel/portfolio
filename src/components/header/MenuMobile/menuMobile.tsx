89056b13789bfe0ae25198553eab45e0360f2830
YannGabriel <yanngabrieldev@gmail.com>
2025-01-11 16:05:22 -0300

    put new animations for navbar with motion library

--- src/components/header/MenuMobile/menuMobile.tsx
+++ src/components/header/MenuMobile/menuMobile.tsx
@@ -1,35 +1,46 @@
 import { useEffect, useState } from "react";
-import menuData from "../../../database/api/menuData.json"
+import { motion } from 'framer-motion';
+import menuData from "../../../database/api/menuData.json";
 import styles from "./menuMobile.module.scss";
 
-
-interface SectionProps{
+interface SectionProps {
   image?: string;
-  name?: string
-  text?: string
-  closeMenu?: () => void
+  name?: string;
+  text?: string;
+  closeMenu?: () => void;
 }
 
-export const MobileMenu = ({closeMenu}: SectionProps) => {
+export const MobileMenu = ({ closeMenu }: SectionProps) => {
 
-  const [sections, setSection] = useState<SectionProps[]>([])
+  const [sections, setSections] = useState<SectionProps[]>([]);
 
   useEffect(() => {
-    setSection(menuData);
+    setSections(menuData);
   }, []);
 
   return (
-    <nav className={styles.mobileMenu}>
-      <ul className={styles.sectionsList}>
-        {sections.map((section, index) => (
-          <li className={styles.sections}>
-            <div key={index} className={styles.itemsList} onClick={closeMenu}>
-              <img src={section.image} alt={section.name} />
-              <h2 className={styles.sectionTitle}>{section.text}</h2>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.2, ease: "linear" }}
+    >
+      <nav className={styles.mobileMenu}>
+        <ul className={styles.sectionsList}>
+          {sections.map((section, index) => (
+            <li key={index} className={styles.sections}>
+              <motion.div 
+              className={styles.itemsList} 
+              onClick={closeMenu}
+              initial = {{y: -30 , opacity: 0}}
+              animate = {{y: 0, opacity: 1}}
+              transition={{duration: 0.3, delay:0.2 + (index * 0.1), ease: "linear"}}>
+                {section.image && <img src={section.image} alt={section.name || 'Section'} />}
+                {section.text && <h2 className={styles.sectionTitle}>{section.text}</h2>}
+              </motion.div>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </motion.div>
   );
 };
