3636cf22a532377bf47b60d2c9fe393af9e3f98a
YannGabriel <yanngabrieldev@gmail.com>
2025-01-12 23:02:16 -0300

    update header link to projects

--- src/components/Projects/projects.tsx
+++ src/components/Projects/projects.tsx
@@ -1,28 +1,28 @@
-
 //Styles
-import styles from "./projects.module.scss"
-import {motion} from "framer-motion"
+import styles from "./projects.module.scss";
+import { motion } from "framer-motion";
 
 //Components
-import { Project } from "./project.tsx/project"
+import { Project } from "./project.tsx/project";
 
-export const Projects = () =>{
-  return(
-    <motion.section 
-    initial = {{y: 50, opacity: 0}}
-    whileInView={{ opacity: 1, y: 0 }}
-    viewport={{ once: true }}
-    transition={{ duration: 0.2, delay: 0.1, ease: "linear" }}
-    className={styles.projectsSection}>
-
+export const Projects = () => {
+  return (
+    <motion.section
+    id="projects"
+      initial={{ y: 50, opacity: 0 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.2, delay: 0.1, ease: "linear" }}
+      className={styles.projectsSection}
+    >
       <h1 className={styles.sectionTitle}>
         Projetos<span className={styles.blackDetail}>:</span>
       </h1>
 
       <p className={styles.projectsIntro}>
-      Veja sobre os projetos que desenvolvi!
+        Veja sobre os projetos que desenvolvi!
       </p>
-    <Project/>
+      <Project />
     </motion.section>
-  )
-}
+  );
+};
